96cabd22136f7601c001034e50187d71ef3c6c6e
ptoney514 <53410556+ptoney514@users.noreply.github.com>
2025-06-25 16:09:27 -0500

    Add full React + Tailwind dashboard project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import I9HealthDashboard from './components/I9HealthDashboard';
+import './components/I9Dashboard.css';
+
+function App() {
+  return (
+    <div className="App">
+      <I9HealthDashboard />
+    </div>
+  );
+}
+
+export default App;
